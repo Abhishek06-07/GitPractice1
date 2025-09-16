26f4d6900a0527c1afc030b01af5ac45affac987
Abhishek Yadav <abhishekyadav809040@gmail.com>
2025-09-16 06:13:53 +0530

    Version12

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
 print("Hello learners of Git it is our first version")
 print("Hello learners of Git it is our second version")
 print("Hello learners of Git it is our third version")
+print("this is 12 version")
---
+++ p2.py
@@ -0,0 +1,2 @@
+print("This is second file")
+print("This is 12 version")
